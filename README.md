687dff8a4b4dc0aa5300d85cfc8bc6d8e8c0e529
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2024-10-03 18:16:36 -0500

    made the plugin to be dynamic per second frequency

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # terrainLoaderPlugin
 
+### Commands in hand
+```
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/home/uav/Documents/PX4-Autopilot/build/px4_sitl_default/build_gazebo-classic
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/home/uav/Documents/terrainLoaderPlugin/build/lib
+
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/uav/Documents/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/uav/Desktop/sdf_maker
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/uav/Documents/PX4-Autopilot/build/px4_sitl_default/build_gazebo-classic
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/uav/Documents/terrainLoaderPlugin/build/lib
+
+gzserver /home/uav/Documents/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds/empty.world --verbose
+
+gz model --verbose --spawn-file="/home/uav/Documents/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/typhoon_h480/typhoon_h480.sdf" --model-name=typhoon_h480 -x 1.01 -y 0.98 -z 0.83
+
+```
--- gazeboTerrainLoader/src/gazebo_terrain_loader.cpp
+++ gazeboTerrainLoader/src/gazebo_terrain_loader.cpp
@@ -1,6 +1,7 @@
 #include <gazebo/gazebo.hh>                // for accessing all gazebo classes
 #include <gazebo/common/common.hh>         // for common fn in gazebo like ModelPlugin
 #include <gazebo/physics/physics.hh>       // for gazebo physics, to access -- ModelPtr
+#include <time.h>
 
 namespace gazebo {
     class SubscriberGazeboPlugin : public ModelPlugin {
@@ -34,19 +35,29 @@ namespace gazebo {
       public:
        void On_msg(ConstPosesStampedPtr& _msg)
         {
+
           // Dump the message contents to stdout.
           // std::cout << _msg->DebugString();
-           for (int i = 0; i < _msg->pose_size(); i++) {
-            std::string msg_name = _msg->pose(i).name();
 
-            // this is the drone pose 
-            if (msg_name.find("::") == std::string::npos) {
-                double x = _msg->pose(i).position().x();
-                double y = _msg->pose(i).position().y();
-                double z = _msg->pose(i).position().z();
-                this->bringUpStaticBlockof1Meter(x,y,z);
+          // execute for every second
+          time_t tempSeconds = time (NULL);
+          
+          if(tempSeconds > this->seconds){
+            this->seconds = tempSeconds;
+
+            for (int i = 0; i < _msg->pose_size(); i++) {
+              std::string msg_name = _msg->pose(i).name();
+
+              // this is the drone pose 
+              if (msg_name.find("::") == std::string::npos) {
+                  double x = _msg->pose(i).position().x();
+                  double y = _msg->pose(i).position().y();
+                  double z = _msg->pose(i).position().z();
+                  this->bringUpStaticBlockof1Meter(x,y,z);
+              }
             }
-           }
+
+          }
 
         }
 
@@ -64,13 +75,21 @@ namespace gazebo {
 
             // Send the message
             this->publisher->Publish(msg);
+
+            std::cout << "spawning the plane at position " << x_position << " - Xposition and " << y_position << " - Yposition!" << std::endl;
+
         }
 
      // good pratices to declare pub/sub as data member
      // reather than declaring them as local varibales which may cause issues
     private:
         transport::SubscriberPtr sub;
+
+    private:
         transport::PublisherPtr publisher;
+
+    private:
+        time_t seconds = time (NULL);
 
     };
     // Register plugin 
