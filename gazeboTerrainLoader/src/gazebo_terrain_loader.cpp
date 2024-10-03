30e5440328153305706569dd039d7326e7a6ebbb
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2024-10-03 16:54:38 -0500

    working code based on pub sub event listening

--- gazeboTerrainLoader/src/gazebo_terrain_loader.cpp
+++ gazeboTerrainLoader/src/gazebo_terrain_loader.cpp
@@ -3,59 +3,76 @@
 #include <gazebo/physics/physics.hh>       // for gazebo physics, to access -- ModelPtr
 
 namespace gazebo {
-    class SubscriberGazeboPlugin : public WorldPlugin {
+    class SubscriberGazeboPlugin : public ModelPlugin {
       public:
-        SubscriberGazeboPlugin() : WorldPlugin() {
+        SubscriberGazeboPlugin() : ModelPlugin() {
             printf("Subscriber Plugin Created!\n");
         }
 
       public:
-        void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) {
+        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
             
-            // using _world pointer & GetName()to get model name
-            std::cout << "World Name = " << _world->Name() << std::endl;
+            // using _model pointer & GetName()to get model name
+            std::cout << "World Name = " << _model->GetName() << std::endl;
 
             // set a node to subscribe
             transport::NodePtr node(new transport::Node());
             node->Init();
             
             std::map<std::string, std::list<std::string>> topicsList = transport::getAdvertisedTopics();
-
             std::cout << "topic number" << topicsList.size() << std::endl;
-
-            for (auto const& x : topicsList)
-                {
-                    std::cout << x.first  // string (key)
-                            << ':' 
-                            << std::endl;
-                    std::cout << "------------------------------" << std::endl;
-
-                    for (auto const& y: x.second) {
-                        std::cout << y << std::endl; 
-                    }
-
-                    std::cout << "==============================" << std::endl;
-
-                }
 
             // subscribe to topic 
             //transport::SubscriberPtr subscribe
-            this->sub = node->Subscribe("~/pose/info", &SubscriberGazeboPlugin::On_msg, this);
+            this->sub = node->Subscribe("~/pose/local/info", &SubscriberGazeboPlugin::On_msg, this);
+
+            // set publisher
+            this->publisher = node->Advertise<msgs::Factory>("~/factory");
+
         }
     
       public:
-       void On_msg(ConstWorldStatisticsPtr &_msg)
+       void On_msg(ConstPosesStampedPtr& _msg)
         {
           // Dump the message contents to stdout.
-          std::cout << _msg->DebugString();  //print output in the terminal
-          
+          // std::cout << _msg->DebugString();
+           for (int i = 0; i < _msg->pose_size(); i++) {
+            std::string msg_name = _msg->pose(i).name();
+
+            // this is the drone pose 
+            if (msg_name.find("::") == std::string::npos) {
+                double x = _msg->pose(i).position().x();
+                double y = _msg->pose(i).position().y();
+                double z = _msg->pose(i).position().z();
+                this->bringUpStaticBlockof1Meter(x,y,z);
+            }
+           }
+
+        }
+
+      public:
+        void bringUpStaticBlockof1Meter(double& x_position, double& y_position, double& z_position)
+        {
+            // create msg obj
+            msgs::Factory msg;
+
+            // model to use
+            msg.set_sdf_filename("model://terrainLoaderBlock");
+
+            // set model pose
+            msgs::Set(msg.mutable_pose(), ignition::math::Pose3d(x_position, y_position, 0.0, 0.0, 0.0, 0.0));
+
+            // Send the message
+            this->publisher->Publish(msg);
         }
 
      // good pratices to declare pub/sub as data member
      // reather than declaring them as local varibales which may cause issues
     private:
-        transport::SubscriberPtr sub; 
+        transport::SubscriberPtr sub;
+        transport::PublisherPtr publisher;
+
     };
     // Register plugin 
-    GZ_REGISTER_WORLD_PLUGIN(SubscriberGazeboPlugin)
+    GZ_REGISTER_MODEL_PLUGIN(SubscriberGazeboPlugin)
 }
